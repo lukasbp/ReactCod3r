8d72b53af2a79c206d6c346b3a00b36ee88f30ae
lukasbp <44759049+lukasbp@users.noreply.github.com>
2020-04-29 03:36:55 -0300

    commit final do projeto TODOAPP acompanhando as aulas do cod3r

---
+++ todoAPP/frontend/src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'>
+            <h2>Nossa História</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, quae tenetur omnis tempora molestias cumque magnam rerum eos maxime autem, porro ea. Iure officiis dolorum facere, repellendus atque quam esse?</p>
+        </PageHeader>
+    </div>
+)
